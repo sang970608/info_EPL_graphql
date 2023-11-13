60d6826a79f4eba27ee87cc8166f6ac265af7484
DESKTOP-A5S91BV\NHWM <sang970608>
2023-11-13 16:57:34 +0900

    4위팀까지 선수정보 추가

--- graphql/db.js
+++ graphql/db.js
@@ -4,85 +4,25 @@ export let team = [
         teamname : "맨체스터 시티 (Manchester City)",
         stadium: "에티하드 스타디움 (Etihad Stadium)",
         director : "펩 과르디올라 (Josep Guardiola)",
-        players : [
-            {
-                backNo: 2,
-                name: "카일 워커",
-                position: "DF"
-            },{
-                backNo: 3,
-                name: "후벵 디아스",
-                position: "DF"
-            },{
-                backNo: 4,
-                name: "칼빈 필립스",
-                position: "DF"
-            },{
-                backNo: 5,
-                name: "존 스톤스",
-                position: "DF"
-            },{
-                backNo: 6,
-                name: "네이션 아케",
-                position: "DF"
-            },{
-                backNo: 8,
-                name: "마테오 코바치치",
-                position: "MF"
-            },{
-                backNo: 9,
-                name: "엘링 홀란",
-                position: "FW"
-            },{
-                backNo: 10,
-                name: "잭 그릴리쉬",
-                position: "MF"
-            },{
-                backNo: 11,
-                name: "제레미 도쿠",
-                position: "MF"
-            },{
-                backNo: 16,
-                name: "로드리",
-                position: "MF"
-            },{
-                backNo: 17,
-                name: "케빈 더 브라위너",
-                position: "MF",
-                captain: true
-            },{
-                backNo: 18,
-                name: "슈테판 오르테가",
-                position: "GK"
-            },{
-                backNo: 19,
-                name: "훌리안 알바레스",
-                position: "FW"
-            },{
-                backNo: 20,
-                name: "베르나르두 실바",
-                position: "MF"
-            },{
-                backNo: 21,
-                name: "세르히오 고메스",
-                position: "DF"
-            }
-        ]
+        players : mancity
     },{
         id: 2,
         teamname : "토트넘 핫스퍼 (Tottenham Hotspur)",
         stadium: "토트넘 핫스퍼 스타디움 (Tottenham Hotspur Stadium)",
-        director : "엔지 포스테코글루 (Ange Postecoglou)"
+        director : "엔지 포스테코글루 (Ange Postecoglou)",
+        players : tottenham
     },{
         id: 3,
         teamname : "리버풀 (Liverpool)",
         stadium: "안필드 (Anfield)",
-        director : "위르겐 클롭 (Jürgen Klopp)"
+        director : "위르겐 클롭 (Jürgen Klopp)",
+        players : liverpool
     },{
         id: 4,
         teamname : "아스날 (Arsenal)",
         stadium: "에미레이트 스타디움 (Emirates Stadium)",
-        director : "미켈 아르테타 (Mikel Arteta)"
+        director : "미켈 아르테타 (Mikel Arteta)",
+        players : arsenal
     },{
         id: 5,
         teamname : "첼시 (Chelsea)",
@@ -165,6 +105,454 @@ export let team = [
         director : "라이언 크레스웰(Ryan Cresswell)"
     }
 ];
+
+export let mancity = [
+    {
+        backNo: 2,
+        name: "카일 워커",
+        position: "DF"
+    },{
+        backNo: 3,
+        name: "후벵 디아스",
+        position: "DF"
+    },{
+        backNo: 4,
+        name: "칼빈 필립스",
+        position: "MF"
+    },{
+        backNo: 5,
+        name: "존 스톤스",
+        position: "DF"
+    },{
+        backNo: 6,
+        name: "네이션 아케",
+        position: "DF"
+    },{
+        backNo: 8,
+        name: "마테오 코바치치",
+        position: "MF"
+    },{
+        backNo: 9,
+        name: "엘링 홀란",
+        position: "FW"
+    },{
+        backNo: 10,
+        name: "잭 그릴리쉬",
+        position: "MF"
+    },{
+        backNo: 11,
+        name: "제레미 도쿠",
+        position: "MF"
+    },{
+        backNo: 16,
+        name: "로드리",
+        position: "MF"
+    },{
+        backNo: 17,
+        name: "케빈 더 브라위너",
+        position: "MF",
+        captain: true
+    },{
+        backNo: 18,
+        name: "슈테판 오르테가",
+        position: "GK"
+    },{
+        backNo: 19,
+        name: "훌리안 알바레스",
+        position: "FW"
+    },{
+        backNo: 20,
+        name: "베르나르두 실바",
+        position: "MF"
+    },{
+        backNo: 21,
+        name: "세르히오 고메스",
+        position: "DF"
+    },{
+        backNo: 24,
+        name: "요슈코 그바르디올",
+        position: "DF"
+    },{
+        backNo: 25,
+        name: "마누엘 아칸지",
+        position: "DF"
+    },{
+        backNo: 27,
+        name: "마테우스 누네스",
+        position: "MF"
+    },{
+        backNo: 31,
+        name: "에데르송 모라에스",
+        position: "GK"
+    },{
+        backNo: 33,
+        name: "스콧 카슨",
+        position: "GK"
+    },{
+        backNo: 47,
+        name: "필 포든",
+        position: "MF"
+    },{
+        backNo: 52,
+        name: "오스카르 보보",
+        position: "MF"
+    },{
+        backNo: 82,
+        name: "리코 루이스",
+        position: "DF"
+    }
+]
+
+export let tottenham = [
+    {
+        backNo: 1,
+        name: "위고 요리스",
+        position: "GK"
+    },{
+        backNo: 4,
+        name: "올리버 스킵",
+        position: "MF"
+    },{
+        backNo: 5,
+        name: "피에르에밀 호이비에르",
+        position: "MF"
+    },{
+        backNo: 7,
+        name: "손흥민",
+        position: "FW",
+        captain : true
+    },{
+        backNo: 8,
+        name: "이브 비수마",
+        position: "MF"
+    },{
+        backNo: 9,
+        name: "히샬리송",
+        position: "FW"
+    },{
+        backNo: 10,
+        name: "제임스 매디슨",
+        position: "MF"
+    },{
+        backNo: 11,
+        name: "브리안 힐",
+        position: "FW"
+    },{
+        backNo: 12,
+        name: "에메르송 로얄",
+        position: "DF"
+    },{
+        backNo: 13,
+        name: "굴리엘모 비카리오",
+        position: "GK"
+    },{
+        backNo: 14,
+        name: "이반 페리시치",
+        position: "MF"
+    },{
+        backNo: 15,
+        name: "에릭 다이어",
+        position: "DF"
+    },{
+        backNo: 17,
+        name: "크리스티안 로메로",
+        position: "DF"
+    },,{
+        backNo: 18,
+        name: "지오바니 로셀소",
+        position: "MF"
+    },{
+        backNo: 19,
+        name: "라이언 세세뇽",
+        position: "DF"
+    },{
+        backNo: 20,
+        name: "프레이저 포스터",
+        position: "GK"
+    },{
+        backNo: 21,
+        name: "데얀 쿨루셉스키",
+        position: "MF"
+    },{
+        backNo: 22,
+        name: "브레넌 존슨",
+        position: "FW"
+    },{
+        backNo: 23,
+        name: "페드로 포로",
+        position: "DF"
+    },{
+        backNo: 27,
+        name: "마노르 솔로몬",
+        position: "FW"
+    },{
+        backNo: 29,
+        name: "파페 사르",
+        position: "MF"
+    },{
+        backNo: 30,
+        name: "로드리고 벤탄쿠르",
+        position: "MF"
+    },{
+        backNo: 33,
+        name: "벤 데이비스",
+        position: "DF"
+    },{
+        backNo: 35,
+        name: "애슐리 필립스",
+        position: "DF"
+    },{
+        backNo: 36,
+        name: "알레호 벨리스",
+        position: "FW"
+    },{
+        backNo: 37,
+        name: "미키 반더벤",
+        position: "DF"
+    },{
+        backNo: 38,
+        name: "데스티니 우도기",
+        position: "DF"
+    },{
+        backNo: 40,
+        name: "브랜던 오스틴",
+        position: "GK"
+    },{
+        backNo: 41,
+        name: "알피 화이트먼",
+        position: "GK"
+    }
+]
+
+export let liverpool = [
+    {
+        backNo: 1,
+        name: "알리송 베케르",
+        position: "GK"
+    },{
+        backNo: 2,
+        name: "조 고메즈",
+        position: "DF"
+    },{
+        backNo: 3,
+        name: "엔도 와타루",
+        position: "MF"
+    },{
+        backNo: 4,
+        name: "버질 반 다이크",
+        position: "DF",
+        captain: true
+    },{
+        backNo: 5,
+        name: "이브라히마 코나테",
+        position: "DF"
+    },{
+        backNo: 6,
+        name: "티아고 알칸타라",
+        position: "MF"
+    },{
+        backNo: 7,
+        name: "루이스 디아스",
+        position: "FW"
+    },{
+        backNo: 8,
+        name: "소보슬라이",
+        position: "MF"
+    },{
+        backNo: 9,
+        name: "다르윈 누녜스",
+        position: "FW"
+    },{
+        backNo: 10,
+        name: "알렉시스 맥 알리스터",
+        position: "MF"
+    },{
+        backNo: 11,
+        name: "모하메드 살라",
+        position: "FW"
+    },{
+        backNo: 13,
+        name: "아드리안 산미겔",
+        position: "GK"
+    },{
+        backNo: 17,
+        name: "커티스 존스",
+        position: "MF"
+    },{
+        backNo: 18,
+        name: "코디 각포",
+        position: "FW"
+    },{
+        backNo: 19,
+        name: "하비 엘리엇",
+        position: "MF"
+    },{
+        backNo: 20,
+        name: "디오구 조타",
+        position: "FW"
+    },{
+        backNo: 21,
+        name: "코스타스 치미카스",
+        position: "DF"
+    },{
+        backNo: 26,
+        name: "앤디 로버트슨",
+        position: "DF"
+    },{
+        backNo: 32,
+        name: "조엘 마팁",
+        position: "DF"
+    },{
+        backNo: 38,
+        name: "라이언 흐라벤베르흐",
+        position: "MF"
+    },{
+        backNo: 42,
+        name: "바비 클라크",
+        position: "FW"
+    },{
+        backNo: 43,
+        name: "스테판 바이체티치",
+        position: "MF"
+    },{
+        backNo: 45,
+        name: "마르셀루 피탈루가",
+        position: "GK"
+    },{
+        backNo: 49,
+        name: "케이드 고든",
+        position: "FW"
+    },{
+        backNo: 50,
+        name: "벤 도크",
+        position: "FW"
+    },{
+        backNo: 56,
+        name: "비테슬라프 야로스",
+        position: "GK"
+    },{
+        backNo: 62,
+        name: "퀴빈 켈러허",
+        position: "GK"
+    },{
+        backNo: 66,
+        name: "알렉산더 아놀드",
+        position: "DF"
+    },{
+        backNo: 78,
+        name: "자렐 콴사",
+        position: "DF"
+    },{
+        backNo: 84,
+        name: "코너 브래들리",
+        position: "DF"
+    }
+]
+
+export let arsenal = [
+    {
+        backNo: 1,
+        name: "아론 램스데일",
+        position: "GK"
+    },{
+        backNo: 2,
+        name: "윌리엄 살리바",
+        position: "DF"
+    },{
+        backNo: 4,
+        name: "벤 화이트",
+        position: "DF"
+    },{
+        backNo: 5,
+        name: "토마스 파티",
+        position: "MF"
+    },{
+        backNo: 6,
+        name: "가브리엘 마갈량이스",
+        position: "DF"
+    },{
+        backNo: 7,
+        name: "부카요 사카",
+        position: "FW"
+    },{
+        backNo: 8,
+        name: "마르틴 외데고르",
+        position: "MF",
+        captain: true
+    },{
+        backNo: 9,
+        name: "가브리엘 제주스",
+        position: "FW"
+    },{
+        backNo: 10,
+        name: "에밀 스미스 로우",
+        position: "MF"
+    },{
+        backNo: 11,
+        name: "가브리엘 마르티넬리",
+        position: "FW"
+    },{
+        backNo: 12,
+        name: "위리엔 팀버르",
+        position: "DF"
+    },{
+        backNo: 14,
+        name: "에디 은케티아",
+        position: "FW"
+    },{
+        backNo: 15,
+        name: "야쿠프 키비오루",
+        position: "DF"
+    },{
+        backNo: 17,
+        name: "세드리크 소아르스",
+        position: "DF"
+    },{
+        backNo: 18,
+        name: "토미야스 타케히로",
+        position: "DF"
+    },{
+        backNo: 19,
+        name: "레안드로 트로사르",
+        position: "MF"
+    },{
+        backNo: 20,
+        name: "조르지뉴",
+        position: "MF"
+    },{
+        backNo: 21,
+        name: "파비우 비에이라",
+        position: "MF"
+    },{
+        backNo: 22,
+        name: "다비드 라야",
+        position: "GK"
+    },{
+        backNo: 24,
+        name: "리스 넬슨",
+        position: "FW"
+    },{
+        backNo: 25,
+        name: "모하메드 엘네니",
+        position: "MF"
+    },{
+        backNo: 29,
+        name: "카이 하베르츠",
+        position: "MF"
+    },{
+        backNo: 31,
+        name: "칼 야콥 헤인",
+        position: "GK"
+    },{
+        backNo: 35,
+        name: "알렉산드르 진첸코",
+        position: "DF"
+    },{
+        backNo: 41,
+        name: "데클런 라이스",
+        position: "MF"
+    }
+]
 
 export const getTeams = () => teams;
 
